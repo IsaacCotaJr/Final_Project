55e9856551b02e107573dff7845f0edd5c759e70
IsaacCotaJr <166754687+IsaacCotaJr@users.noreply.github.com>
2025-04-03 10:07:43 -0700

    Update Deck.java

--- main/model/Deck.java
+++ main/model/Deck.java
@@ -1,5 +1,47 @@
+
 package model;
 
+import java.util.*;
+
 public class Deck {
+
+	private final List<Card> cards;
+
+	public Deck() {
+		cards = new ArrayList<>();
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				cards.add(new Card(rank, suit));
+			}
+		}
+
+	}
+
+	public List<Card> getDeck() {
+		return cards;
+	}
+
+	public List<Card> shuffle() {
+		Collections.shuffle(cards);
+		return cards;
+	}
+
+	public List<Card> dealCommunityCards() {
+		// TODO Auto-generated method stub
+		List<Card> communityCards = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			communityCards.add(cards.remove(0));
+		}
+		return communityCards;
+	}
+
+	public List<Card> dealPlayerCards() {
+		// TODO Auto-generated method stub
+		List<Card> playerCards = new ArrayList<>();
+		for (int i = 0; i < 2; i++) {
+			playerCards.add(cards.remove(0));
+		}
+		return playerCards;
+	}
 
 }
